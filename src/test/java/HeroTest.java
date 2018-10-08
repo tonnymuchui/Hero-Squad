854c4cd4de5dc96d4239dd8add4be9f6b4bf379d
tonnymuchui <tonykanyingah@gmail.com>
2018-10-08 12:10:20 +0300

    final changes

--- src/test/java/HeroTest.java
+++ src/test/java/HeroTest.java
@@ -10,57 +10,57 @@ public class HeroTest {
 
     @Test
     public void Hero() {
-        Hero myHero = new Hero("Batman", 25, "X-Ray versions","rely on sun for energy");
+        Hero myHero = new Hero("Batman", 25, "X-Ray versions","energy");
         assertEquals(true,myHero instanceof Hero);
     }
 
     @Test
     public void name(){
-        Hero myHero = new Hero("Batman", 25, "X-Ray versions","rely on sun for energy");
+        Hero myHero = new Hero("Batman", 25, "X-Ray versions","energy");
    assertEquals("Batman", myHero.getName());
     }
 
     @Test
     public void age(){
-        Hero myHero = new Hero("Batman", 25, "X-Ray versions","rely on sun for energy");
+        Hero myHero = new Hero("Batman", 25, "X-Ray versions","energy");
         assertEquals(25, myHero.getAge());
     }
 
     @Test
     public void power(){
-        Hero myHero = new Hero("Batman", 25, "X-Ray versions","rely on sun for energy");
+        Hero myHero = new Hero("Batman", 25, "X-Ray versions","energy");
         assertEquals("X-Ray versions", myHero.getPower());
     }
 
     @Test
     public void weakness(){
-        Hero myHero = new Hero("Batman", 25, "X-Ray versions","rely on sun for energy");
-        assertEquals("rely on sun energy", myHero.getWeakness());
+        Hero myHero = new Hero("Batman", 25, "X-Ray versions","energy");
+        assertEquals("energy", myHero.getWeakness());
     }
 
     @Test
     public void iCompleted(){
-        Hero myHero = new Hero("Batman", 25, "X-Ray versions","rely on sun for energy");
+        Hero myHero = new Hero("Batman", 25, "X-Ray versions","energy");
         assertFalse( myHero.ismCompleted());
     }
 
     @Test
     public void time(){
-        Hero myHero = new Hero("Batman", 25, "X-Ray versions","rely on sun for energy");
+        Hero myHero = new Hero("Batman", 25, "X-Ray versions","energy");
         assertEquals(LocalDateTime.now().getDayOfWeek(), myHero.getmCreatedAt().getDayOfWeek());
     }
 
     @Test
     public void all() {
-        Hero firstHero = new Hero("Batman", 25, "X-Ray versions","rely on sun for energy");
-        Hero secondHero = new Hero("Batman", 25, "X-Ray versions","rely on sun for energy");
+        Hero firstHero = new Hero("Batman", 25, "X-Ray versions","energy");
+        Hero secondHero = new Hero("Batman", 25, "X-Ray versions","energy");
         assertTrue(Hero.all().contains(firstHero));
         assertTrue(Hero.all().contains(secondHero));
     }
 
     @Test
     public void clear(){
-        Hero myHero = new Hero("Batman", 25, "X-Ray versions","rely on sun for energy");
+        Hero myHero = new Hero("Batman", 25, "X-Ray versions","energy");
         Hero.clear();
         assertEquals(Hero.all().size(), 0);
     }
@@ -68,14 +68,14 @@ public class HeroTest {
     @Test
    public void getId(){
         Hero.clear();
-        Hero myHero = new Hero("Batman", 25, "X-Ray versions","rely on sun for energy");
+        Hero myHero = new Hero("Batman", 25, "X-Ray versions","energy");
         assertEquals(1,myHero.getId());
     }
 
     @Test
     public void find() {
-        Hero firstHero = new Hero("Batman", 25, "X-Ray versions","rely on sun for energy");
-        Hero secondHero = new Hero("Batman", 25, "X-Ray versions","rely on sun for energy");
+        Hero firstHero = new Hero("Batman", 25, "X-Ray versions","energy");
+        Hero secondHero = new Hero("Batman", 25, "X-Ray versions","energy");
         assertEquals(Hero.find(secondHero.getId()), secondHero);
     }
     }
